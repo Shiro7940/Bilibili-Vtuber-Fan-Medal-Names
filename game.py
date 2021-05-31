0423ed63071ad394b8e98b733807339accf0c5cb
Shiro <70893653+Shiro7940@users.noreply.github.com>
2021-05-31 21:04:14 +0800

    Add files via upload

---
+++ game.py
@@ -0,0 +1,121 @@
+from main import *
+import random
+
+all_medal = set(vtb_fan_medal_dict.values())
+all_medal.remove("粉丝团")
+all_medal.remove("暂无粉丝勋章")
+all_medal_lst = list(all_medal)
+
+def get_name_by_medal(mname:str):
+    try:
+        name = []
+        roomid = find_rid_by_medal(mname)
+        namedict = gen_rid_to_name_dict()
+        for item in roomid:
+            nametemp = namedict.get(item,"Name Not Found")
+            name += [nametemp]
+        return name[0]
+    except Exception as error:
+        print(error)
+        print("An ERROR Occurred")
+        return None
+
+def gen_question_set(mname:str):
+    names = []
+    medals = []
+    output = set()
+    i = 0
+    while i in range(0,3):
+        temp = all_medal_lst[random.randint(0,len(all_medal_lst)-1)]
+        if temp != mname:
+            if get_name_by_medal(temp) != 'Name Not Found':
+                medals += [temp]
+                names += [get_name_by_medal(temp)]
+                i += 1
+    names += [get_name_by_medal(mname)]
+    medals += [mname]
+    for n in range(0,len(names)):
+        output.add((names[n],medals[n]))
+    name1 = 1
+    return output
+    
+def gen_question(qset:set,ans_name:str,ans_medal:str,mode:int) -> bool:
+    try:
+        if mode == 0:
+            print("下列哪个虚拟UP主的勋章是 '"+ans_medal+"':")
+        elif mode == 1:
+            print("虚拟UP主 '"+ans_name+"' 的勋章是: ")
+        else:
+            print("模式选择无效")
+            return False
+        qlist = list(qset)
+        a = qlist[0][mode]
+        b = qlist[1][mode]
+        c = qlist[2][mode]
+        d = qlist[3][mode]
+        templist = [a,b,c,d]
+        ans_dict = {"A":0,"B":1,"C":2,"D":3}
+        print("A: "+str(a)+
+              "\nB: "+str(b)+
+              "\nC: "+str(c)+
+              "\nD: "+str(d))
+        answer = str(input("答案: ")).upper()
+        if answer == "E":
+            return "Exit"
+        c = ans_dict.get(answer)
+        if mode == 1 and templist[c] == ans_medal:
+            print("回答正确")
+            return True
+        elif mode == 0 and templist[c] == ans_name:
+            print("回答正确")
+            return True
+        else:
+            if mode == 1:
+                print("回答错误，答案是 '"+ans_medal+"'")
+            if mode == 0:
+                print("回答错误，答案是 '"+ans_name+"'")
+            return False
+    except:
+        print("答案无效")
+        return False
+
+def game():
+    exit = False
+    correct = 0
+    false = 0
+    total = 0 
+    print("回答E可退出")
+    try:
+        limit = int(input("请输入题目量: "))
+    except:
+        limit = 2000
+    try:
+        while (not exit) and (len(all_medal) >= 4) and total<limit: 
+            ans_medal = all_medal.pop()
+            ans_name = get_name_by_medal(ans_medal)
+            if ans_name != 'Name Not Found':
+                total+=1
+                print("第 "+str(total)+"/"+str(limit)+" 题")
+                qset = gen_question_set(ans_medal)
+                mode = round(random.random())
+                status = gen_question(qset,ans_name,ans_medal,mode)
+                if status == True:
+                    correct += 1
+                elif status == False:
+                    false += 1
+                elif status == "Exit":
+                    exit = True
+                    total-=1
+                print("-------------------")
+            else:
+                pass
+            
+    finally:
+        score_percent = "NaN"
+        if total != 0 :
+            score_percent = str(round((correct/total)*100,4))
+        print("你的分数是: "+score_percent)
+        print("正确 "+str(correct)+"题 错误 "+str(false)+"题 总计 "+str(total)+"题")
+        
+if __name__ == "__main__":
+    game()
